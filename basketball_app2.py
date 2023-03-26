a56254b87da886584e3429e70eb8060a3f99bdaa
michaelvenier <98124410+michaelvenier@users.noreply.github.com>
2023-03-26 12:51:40 -0700

    Update basketball_app2.py

--- basketball_app2.py
+++ basketball_app2.py
@@ -26,7 +26,7 @@ This is my model. Here's how to use it.
 
 #SECTION: GETTING, CLEANING AND FILTERING DATA
 
-@st.cache(suppress_st_warning=True) #recent
+@st.cache(hash_funcs=True) #recent
 def load_data2(): #recent
     # Create a connection object.
     credentials = service_account.Credentials.from_service_account_info(
