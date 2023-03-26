f7266ac8c1716003dfeed1811d521713639d42ec
michaelvenier <98124410+michaelvenier@users.noreply.github.com>
2023-03-25 23:48:24 -0700

    Update basketball_app2.py

--- basketball_app2.py
+++ basketball_app2.py
@@ -32,7 +32,7 @@ query = f'SELECT * FROM "{sheet_url}"'
 rows = conn.execute(query, headers=1).fetchall()
 
 # Convert data to a Pandas DataFrame.
-df = pd.DataFrame.from_records(rows[2:], columns=rows[1])
+df = pd.DataFrame.from_records(rows[0:], columns=rows[0])
 
 # Print the DataFrame.
 st.dataframe(df)
